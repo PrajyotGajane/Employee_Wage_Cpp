896a43b1f04d9fd14b43324034a78dc97960078e
PrajyotGajane <prajyotgajane@gmail.com>
2020-08-26 02:36:12 +0530

    Refactored: Used switch case by replacing if-else

--- EmployeeWage.cpp
+++ EmployeeWage.cpp
@@ -6,30 +6,41 @@ const int RATE_PER_HOUR = 20;
 const int FULL_DAY_WORK = 1;
 const int PART_DAY_WORK = 2;
 
-class Employee{
+class Employee
+{
     int empHours;
-    public :
-        int checkAttendance();
-        int getDailyWage(int);
+
+public:
+    int checkAttendance();
+    int getDailyWage(int);
 };
 
-int Employee :: checkAttendance(){
+int Employee ::checkAttendance()
+{
     srand(time(0));
-    return (rand() % 3);  
+    return (rand() % 3);
 }
 
-int Employee :: getDailyWage(int attendance){
-    if (attendance == FULL_DAY_WORK)
+int Employee ::getDailyWage(int attendance)
+{
+    switch (attendance)
+    {
+    case FULL_DAY_WORK:
         empHours = 8;
-    else if (attendance == PART_DAY_WORK)
-            empHours = 4;
-    else        
+        break;
+    case PART_DAY_WORK:
+        empHours = 4;
+        break;
+    default:
         empHours = 0;
-   return empHours * RATE_PER_HOUR;
+    }
+    return empHours * RATE_PER_HOUR;
 }
 
-int main(){
-    cout << "\nWelcome To Employee Wage Computation\n" << endl;
+int main()
+{
+    cout << "\nWelcome To Employee Wage Computation\n"
+         << endl;
     Employee empObj;
     int attendance = empObj.checkAttendance();
     cout << "Employee's today's wage is : " << empObj.getDailyWage(attendance) << endl;
