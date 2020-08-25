203111e9e34262ad1bd81730cdcf425b0af144ec
PrajyotGajane <prajyotgajane@gmail.com>
2020-08-26 03:04:44 +0530

    Added: Condtion for employee to work for a total of 20 days or 100 hours

--- EmployeeWage.cpp
+++ EmployeeWage.cpp
@@ -6,6 +6,7 @@ const int RATE_PER_HOUR = 20;
 const int FULL_DAY_WORK = 1;
 const int PART_DAY_WORK = 2;
 const int TOTAL_WORKING_DAYS = 20;
+const int TOTAL_WORKING_HOURS = 100;
 
 class Employee
 {
@@ -13,7 +14,7 @@ class Employee
 
 public:
     int checkAttendance();
-    int getDailyWage(int);
+    int getDailyHours(int);
 };
 
 int Employee ::checkAttendance()
@@ -21,7 +22,7 @@ int Employee ::checkAttendance()
     return (rand() % 3);
 }
 
-int Employee ::getDailyWage(int attendance)
+int Employee ::getDailyHours(int attendance)
 {
     switch (attendance)
     {
@@ -34,7 +35,7 @@ int Employee ::getDailyWage(int attendance)
     default:
         empHours = 0;
     }
-    return empHours * RATE_PER_HOUR;
+    return empHours;
 }
 
 int main()
@@ -44,15 +45,18 @@ int main()
          << endl;
     Employee empObj;
     int days = 0;
+    int totalHours = 0;
     int totalWage = 0;
-    while (days < TOTAL_WORKING_DAYS)
+    while (days < TOTAL_WORKING_DAYS && totalHours < TOTAL_WORKING_HOURS)
     {
         int attendance = empObj.checkAttendance();
-        int dailyWage = empObj.getDailyWage(attendance);
-        totalWage += dailyWage;
-        cout << "Day :" << (days + 1) << "  Wage : " << dailyWage << endl;
+        int dailyHours = empObj.getDailyHours(attendance);
+        totalHours += dailyHours;
+        totalWage += dailyHours * RATE_PER_HOUR;
+        cout << "Day :" << (days + 1) << "  Wage : " << (dailyHours * RATE_PER_HOUR) << endl;
         days++;
     }
+    cout << "Total Hours worked : " << totalHours << endl;
     cout << "Montly wage : " << totalWage << endl;
     return 0;
 }
